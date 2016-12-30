621d90a47709aad6008d4425d2130db70e3fa55f
tksander <tkepasander@gmail.com>
2016-12-29 19:35:32 -0700

    Added deleted note. Cleaned up reverseWords formatting.

--- jasmine/src/reverseWords.js
+++ jasmine/src/reverseWords.js
@@ -1,11 +1,16 @@
-// Write a function reverseWords() that takes a string message and reverses the order of the words in-place 
+/** Write a function reverseWords() that takes a string message and reverses the order of the words in-place 
+*
+* var message = 'find you will pain only go you recordings security the into if';
+* reverseWords(message);
+* returns: 'if into the security recordings you go only pain will you find'
+*/
 
-// var message = 'find you will pain only go you recordings security the into if';
 
-// reverseWords(message);
-// returns: 'if into the security recordings you go only pain will you find'
-
-
+/**
+ * @method reverseWords
+ * @param  { String } message
+ * @returns { String } reversed string of words
+ */
 var reverseWords = function(message) {
   var wordArray = message.split(" ");
   var messageLength = wordArray.length - 1;
