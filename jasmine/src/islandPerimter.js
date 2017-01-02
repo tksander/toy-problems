397a97a9d2384276f9e038857285d3a57532da2d
tksander <tkepasander@gmail.com>
2017-01-01 21:52:59 -0800

    Started IslandPerimeter

---
+++ jasmine/src/islandPerimter.js
@@ -0,0 +1,11 @@
+describe("islandPerimeter", function() {
+
+  beforeEach(function() {
+  })
+
+  describe("islandPerimeter", function() {
+    it("should do something", function() {
+      expect(two.next.val).toEqual(4);
+    });
+  });
+});
