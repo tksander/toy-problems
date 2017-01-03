1d7f0d83497fa0e6f580b875de2bddb7d2679dba
tksander <tkepasander@gmail.com>
2017-01-02 21:46:01 -0800

    Fixed error in islandPerimeter

--- jasmine/spec/islandPerimeter.spec.js
+++ jasmine/spec/islandPerimeter.spec.js
@@ -11,6 +11,14 @@ describe("islandPerimeter", function() {
     });
   });
 
+  describe("islandPerimeter - 2 island", function() {
+    it("should correctly count an island of 1 permimeter", function() {
+      var grid = [[1,1]];
+      var perimeterNum = islandPerimeter(grid);
+      expect(perimeterNum).toEqual(6);
+    });
+  });
+
   describe("islandPerimeter - multiple side island", function() {
     it("should correctly count an island of multiple sides", function() {
       var grid = [[0,1,0,0],
--- jasmine/src/islandPerimeter.js
+++ jasmine/src/islandPerimeter.js
@@ -1,5 +1,5 @@
 /**
- *
+ * Problem prompt:
  * You are given a map in form of a two-dimensional integer grid where 1 represents
  * land and 0 represents water. Grid cells are connected horizontally/vertically
  * (not diagonally). The grid is completely surrounded by water, and there is
@@ -8,40 +8,50 @@
  * water around the island). One cell is a square with side length 1.
  * The grid is rectangular, width and height don't exceed 100.
  * Determine the perimeter of the island.
- *
+ * @see https://leetcode.com/problems/island-perimeter/
  */
 
-var arr = [[0,1,0,0],
-           [1,1,1,0],
-           [0,1,0,0],
-           [1,1,0,0]]
-
-
+/**
+ * Caculate number of sides on island
+ * @method islandPerimeter
+ * @param { number[][] } grid - grid representing islands
+ * @returns { number } perimeter - Perimeter of island
+ */
 var islandPerimeter = function(grid) {
   var perimeter = 0;
   for (var i = 0; i < grid.length; i++) {
      for (var j = 0; j < grid[i].length; j++) {
         if (grid[i][j] === 1) {
           perimeter += 4 - numberConnected(i, j, grid);
-        };
+        }
     }
    }
   return perimeter;
-  }
+  };
 
+/**
+ * Helper function, checks how many sides a piece of land
+ * is connected with other land
+ * @method numberConnected
+ * @param { number } row
+ * @param { number } col
+ * @param { number[][] } grid
+ * @returns number
+ */
 var numberConnected = function(row, col, grid) {
-   var gridExtent = grid.length - 1;
+   var colExtent = grid[0].length - 1;
+   var rowExtent = grid.length - 1;
    var numberSubtract = 0;
    // up
    if (row - 1 >= 0 && grid[row - 1][col] === 1) {
      numberSubtract++;
    }
    // down
-   if (row + 1 <= gridExtent && grid[row  + 1][col] === 1) {
+   if (row + 1 <= rowExtent && grid[row  + 1][col] === 1) {
      numberSubtract++;
    }
    // right
-   if (col + 1 <= gridExtent && grid[row][col + 1] === 1) {
+   if (col + 1 <= colExtent && grid[row][col + 1] === 1) {
      numberSubtract++;
    }
    // left
@@ -49,5 +59,5 @@ var numberConnected = function(row, col, grid) {
      numberSubtract++;
    }
    return numberSubtract;
-}
+};
 // recursively walk through grid
