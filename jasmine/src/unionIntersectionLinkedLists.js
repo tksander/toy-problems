05e8215d7d1f3bfdcf326c44575ac690e1051c43
tksander <tkepasander@gmail.com>
2017-01-05 11:45:36 -0800

    Completed unionIntersectionLinkedLists

--- jasmine/src/unionIntersectionLinkedLists.js
+++ jasmine/src/unionIntersectionLinkedLists.js
@@ -3,6 +3,7 @@
  * Given two Linked Lists, create union and intersection lists that contain union and
  * intersection of the elements present in the given lists.
  * Order of elments in output lists doesn’t matter.
+ * @see http://www.geeksforgeeks.org/union-and-intersection-of-two-linked-lists/
  *
  * Example:
  *
@@ -13,6 +14,71 @@
  *    Intersection List: 4->10
  *    Union List: 2->8->20->4->15->10
  */
+
+/**
+ * Naive solution
+ */
+var intersectionList = function(list1, list2) {
+  var resultList = new SinglyLinkedList();
+  l1current = list1.head;
+  l2current = list2.head;
+
+  while (l1current !== null) {
+    // loop over second list
+    while (l2current !== null) {
+        // if second list item matches first, add to intersection
+        if (l1current.val === l2current.val) {
+            resultList.add(l2current.val);
+        }
+        l2current = l2current.next;
+    }
+    l2current = list2.head;
+    l1current = l1current.next;
+  }
+  return resultList;
+}
+
+/**
+ * Hashed solution
+ */
+var intersectionList = function(list1, list2) {
+  var resultList = new SinglyLinkedList();
+  l1current = list1.head;
+  l2current = list2.head;
+  var table = {};
+
+  while (l1current !== null) {
+    table[l1current.val] = l1current.val
+    l1current = l1current.next;
+  }
+  while (l2current !== null ) {
+     if (table[l2current.val]){
+        resultList.add(l2current.val);
+     }
+    l2current = l2current.next;
+  }
+  return resultList;
+}
+
+var unionList = function(list1, list2) {
+  var resultList = new SinglyLinkedList();
+  l1current = list1.head;
+  l2current = list2.head;
+  var table = {};
+
+  while (l1current !== null) {
+    table[l1current.val] = l1current.val
+    resultList.add(l1current.val);
+    l1current = l1current.next;
+  }
+  while (l2current !== null ) {
+     if (!table[l2current.val]){
+        resultList.add(l2current.val);
+     }
+    l2current = l2current.next;
+  }
+  return resultList;
+}
 
 
 var SinglyLinkedList = function() {
