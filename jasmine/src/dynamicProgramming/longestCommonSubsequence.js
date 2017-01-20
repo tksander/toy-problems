0d863c315b3b784dfb438e182a7dfd9f8c678de7
tksander <tkepasander@gmail.com>
2017-01-20 14:33:21 -0800

    Completed naive solution for longestCommonSubsequence

--- jasmine/src/dynamicProgramming/longestCommonSubsequence.js
+++ jasmine/src/dynamicProgramming/longestCommonSubsequence.js
@@ -17,7 +17,52 @@
  *
  */
 
-// Naive solution
-// generate power set for each string
-// compare power sets and find max length
+/**
+ * Generates the powerSet of a given string
+ * @method powerSet
+ * @param {string} str
+ * @returns {Array<string>}
+ */
+var powerSet = function(str) {
+  var strArr = str.split('');
+  var subStrings = [];
+
+  var recurse = function(subStr, remain) {
+    var currSubstr = subStr;
+    if (remain.length === 0) {
+       return;
+    }
+    for (var i = 0; i < remain.length; i++) {
+      currSubstr.push(remain[i]);
+      subStrings.push(currSubstr.join(''));
+      recurse(currSubstr, remain.slice(i + 1));
+      currSubstr.pop();
+    }
+  };
+  recurse([], strArr);
+  return subStrings;
+};
+
+/**
+ * Naive solution of least common subsequence of two strings
+ * Generates powersets for both strings and compares the sets
+ * to find the longest common string.
+ * @param {string} str1
+ * @param {string} str2
+ * @return {stinrg}
+ */
+var lcsNaive = function(str1, str2) {
+   var subs1 = powerSet(str1);
+   var subs2 = powerSet(str2);
+   var max = '';
+   subs1.forEach(function(subStr1) {
+     subs2.forEach(function(subStr2) {
+        if (subStr1 === subStr2 &&
+            subStr1.length > max.length) {
+          max = subStr1;
+        }
+     });
+   });
+   return max;
+};
 
