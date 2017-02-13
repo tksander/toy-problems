0627697fcea6abdf32c4e25c4e2e54ee7bcf50ef
tksander <tkepasander@gmail.com>
2017-02-13 15:44:19 -0800

    Completed max path sum

---
+++ jasmine/src/trees/maxPathSum.js
@@ -0,0 +1,80 @@
+/**
+ * Maximum Path Sum in a Binary Tree
+ * @see http://www.geeksforgeeks.org/find-maximum-path-sum-in-a-binary-tree/
+ *
+ * Given a binary tree, find the maximum path sum. The path may start and end at any node in the tree.
+ *
+ * Example:
+ *
+ * Input: Root of below tree
+ *        1
+ *       / \
+ *      2   3
+ *     /
+ *    -2
+ *    /
+ *  3
+ * Output: 6
+ *
+ * See below diagram for another example.
+ * 1+2+3
+ *
+ */
+
+    // check which child yields greater (only store if greater than 0)
+    // keep the child with greater yield
+    // check if l + r + n is greater than current greatest
+
+var maxPathSum = function(root) {
+  var max = 0
+  var current = 0
+
+  var recurse = function(node) {
+
+    if (!node) {
+        return 0
+    }
+
+    if (node.left === null && node.right === null) {
+      max = Math.max(max, node.val)
+      return node.val;
+    }
+
+    var left = recurse(node.left)
+    current = 0
+    var right = recurse(node.right)
+    current = 0
+
+    var greaterChild = Math.max(left, right)
+    var immediateFam = left + right + node.val
+    var nodeAndChild = greaterChild + node.val
+
+    max = Math.max(immediateFam, greaterChild, nodeAndChild, node.val, max)
+    current = Math.max(Math.max(greaterChild, nodeAndChild, node.val), 0)
+    return current
+  }
+  recurse(root)
+
+  return max
+}
+
+var Node = function(val) {
+    this.val = val
+    this.left = null;
+    this.right = null;
+}
+
+var r = new Node(1)
+r.left = new Node(2)
+r.left.left = new Node(-2)
+r.right = new Node(3)
+
+var c = new Node(10)
+c.left = new Node(2)
+c.left.left = new Node(20)
+c.left.right = new Node(1)
+c.right = new Node(10)
+c.right.right = new Node(-25)
+c.right.right.left = new Node(3)
+c.right.right.right = new Node(4)
+
---
+++ jasmine/src/trees/minDepthTree.js
@@ -0,0 +1,16 @@
+/**
+ * @see http://www.geeksforgeeks.org/find-minimum-depth-of-a-binary-tree/
+ *
+ * Find Minimum Depth of a Binary Tree
+ * Given a binary tree, find its minimum depth. The minimum depth is the number of nodes along the shortest path from root node down to the nearest leaf node.
+ *
+ * For example, minimum height of below Binary Tree is 2.
+ * Example Tree
+ *
+ * Note that the path must end on a leaf node. For example, minimum height of below Binary Tree is also 2.
+ *
+ *           10
+ *         /
+ *       5
+ */
+
